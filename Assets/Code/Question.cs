b67a6c6e0549ff6fd80655c6d66919c7ac58d132
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-02-13 21:31:52 -0400

    Started project + questions

---
+++ Assets/Code/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Question
+{
+    public string Statement;
+    public bool isDare;
+    public bool Kids, Teens, Adults, Couples, Naughty;
+}
---
+++ Assets/Code/QuestionDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class QuestionDatabase : MonoBehaviour
+{
+    public TextAsset questionsJSON;
+
+    string questionsString;
+    public List<Question> allQuestions = new List<Question>();
+
+    void Start()
+    {
+        questionsString = questionsJSON.text;
+        List<string> questions = questionsString.Split('}').ToList<string>();
+        for (int i = 0; i < questions.Count-1; i++)
+        {
+            questions[i] += "}";
+            allQuestions.Add(JsonUtility.FromJson<Question>(questions[i]));
+        }
+    }
+}
