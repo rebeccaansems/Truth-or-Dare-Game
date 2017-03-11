65a6231c5cf9e2db005c319897443f4e428e6cd7
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-03-10 23:28:53 -0400

    Attempted ads V2

---
+++ Assets/Code/Ads.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+#if UNITY_ADS
+using UnityEngine.Advertisements; // only compile Ads code on supported platforms
+#endif
+
+public class Ads : MonoBehaviour
+{
+    public void ShowAd()
+    {
+#if UNITY_ADS
+        if (!Advertisement.IsReady())
+        {
+            Debug.Log("Ads not ready for default placement");
+            return;
+        }
+
+        Advertisement.Show();
+#endif
+    }
+}
