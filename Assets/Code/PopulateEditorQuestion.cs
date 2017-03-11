4299c9dbdc730aa620727d14d888ea1a05d6ebf2
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-03-10 20:13:31 -0400

    Made questions easier to add/edit/scroll through

--- Assets/Code/PopulateEditorQuestion.cs
+++ Assets/Code/PopulateEditorQuestion.cs
@@ -7,12 +7,7 @@ public class PopulateEditorQuestion : MonoBehaviour
 {
     public InputField statementText;
     public Toggle isKids, isTeens, isAdults, isDare, isNaughty;
-    public Button removeQuestion;
-
-    public void DeleteQuestion()
-    {
-        Destroy(this.gameObject);
-    }
+    public Button removeQuestion, editQuestion, submitQuestion;
 
     private void Update()
     {
@@ -20,5 +15,47 @@ public class PopulateEditorQuestion : MonoBehaviour
         {
             isAdults.isOn = true;
         }
+    }
+
+    private void Start()
+    {
+        submitQuestion.gameObject.SetActive(false);
+        statementText.interactable = false;
+        isKids.interactable = false;
+        isTeens.interactable = false;
+        isAdults.interactable = false;
+        isDare.interactable = false;
+        isNaughty.interactable = false;
+    }
+
+    public void EditQuestion()
+    {
+        editQuestion.gameObject.SetActive(false);
+
+        submitQuestion.gameObject.SetActive(true);
+        statementText.interactable = true;
+        isKids.interactable = true;
+        isTeens.interactable = true;
+        isAdults.interactable = true;
+        isDare.interactable = true;
+        isNaughty.interactable = true;
+    }
+
+    public void SubmitQuestion()
+    {
+        editQuestion.gameObject.SetActive(true);
+
+        submitQuestion.gameObject.SetActive(false);
+        statementText.interactable = false;
+        isKids.interactable = false;
+        isTeens.interactable = false;
+        isAdults.interactable = false;
+        isDare.interactable = false;
+        isNaughty.interactable = false;
+    }
+
+    public void DeleteQuestion()
+    {
+        Destroy(this.gameObject);
     }
 }
